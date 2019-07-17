0c6bf52d924c697f6d4417ab9de773c99d30a2e7
zxx <17242458592qq.com>
2019-07-17 14:07:34 +0800

    将变量封装，让不同的项目读取不同的配置文件

--- src/main/java/com/firstweb/demo/controller/AuthorizeController.java
+++ src/main/java/com/firstweb/demo/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.firstweb.demo.pojo.AccessTokenPOJO;
 import com.firstweb.demo.pojo.GithubUserPOJO;
 import com.firstweb.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +18,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubprovider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String cilentSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenPOJO accessTokenPOJO=new AccessTokenPOJO();
         accessTokenPOJO.setCode(code);
-        accessTokenPOJO.setRedirect_uri("http://localhost:4396/callback");
+        accessTokenPOJO.setRedirect_uri(redirectUri);
         accessTokenPOJO.setState(state);
-        accessTokenPOJO.setClient_id("5968f58600b7ef5001ee");
-        accessTokenPOJO.setClient_secret("8f46f2052e7dba4ad6ad4405d5e4c3cdf453b59a");
+        accessTokenPOJO.setClient_id(clientId);
+        accessTokenPOJO.setClient_secret(cilentSecret);
         String accessToken=githubprovider.getAccessToken(accessTokenPOJO);
         GithubUserPOJO user=githubprovider.getUser(accessToken);
         System.out.println(user.getName());
